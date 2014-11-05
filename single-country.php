3a830c0e8493bc88cfffef32a33ecb481c02dc06
David Fernandez <dfernandez06@gmail.com>
2014-11-05 15:41:38 -0500

    Add optional boxes to ountries

--- single-country.php
+++ single-country.php
@@ -21,39 +21,58 @@
  	<?php endwhile; else : ?>
 	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
+<?php $thispost = $post->ID; ?> 
+
 	</article>
+	<ul class="infoBoxMenu">
+	<?php $infoboxes = get_post_meta( $thispost, 'info_boxes', true ); 
+		$count = 0;
+		foreach($infoboxes as $box): 
+			$title = $box['box_title'];?>
+			<li><a href="#<?php echo $title.$count; ?>"><?php echo $title ?></a></li>
+		<?php $count += 1;
+		endforeach; ?>
+	</ul>
 
-<?php $thispost = $post->ID; ?>
-<h1>Discussion</h1>
-<?php $post_categories = wp_get_post_categories( $post->ID );
-foreach ($post_categories as $key => $value): ?>
 
-<?php $rand_post = new WP_query();
-$rand_post->query('post_type=post&cat=' . $value);
-while($rand_post->have_posts()): $rand_post->the_post();?>
-<?php if((get_post_type() == 'post')&&($post->ID != $thispost)): ?>
-	<h2 class="postTitle"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-<?php endif; ?>
-<?php endwhile; ?>
-<?php wp_reset_query(); ?>
-<?php endforeach; ?>
+<?php $infoboxes = get_post_meta( $thispost, 'info_boxes', true );
+	$count = 0; 
+	foreach($infoboxes as $box): ?>
+	<div id="<?php echo $box['box_title'].$count; ?>" class="countryInfoBox">
+		<h3 class="countryInfoBoxTitle"><?php echo $box['box_title']; ?></h3>
+		<p class="countryInfoBoxContent"><?php echo $box['box_content']; ?></p>
+	</div>
+<?php $count += 1;
+	endforeach; ?>
 
 <h1>Issues</h1>
 <?php $post_categories = wp_get_post_categories( $post->ID );
 foreach ($post_categories as $key => $value): ?>
-<?php $rand_post = new WP_query();
-$rand_post->query('post_type=question&cat='.$value);
+	<?php $rand_post = new WP_query();
+	$rand_post->query('post_type=question&cat='.$value);
 
-while($rand_post->have_posts()): $rand_post->the_post();?>
-<?php if((get_post_type() != 'post')&&($post->ID != $thispost)): ?>
-	<h2 class="postTitle"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-<?php endif; ?>
-<?php endwhile;
-wp_reset_query(); ?>
+	while($rand_post->have_posts()): $rand_post->the_post();?>
+	<?php if((get_post_type() != 'post')&&($post->ID != $thispost)): ?>
+		<h2 class="postTitle"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+	<?php endif; ?>
+	<?php endwhile;
+	wp_reset_query(); ?>
 <?php endforeach; ?>
 
 
+<h1>Discussion</h1>
+<?php $post_categories = wp_get_post_categories( $post->ID );
+	foreach ($post_categories as $key => $value): ?>
 
+	<?php $rand_post = new WP_query();
+	$rand_post->query('post_type=post&cat=' . $value);
+	while($rand_post->have_posts()): $rand_post->the_post();?>
+	<?php if((get_post_type() == 'post')&&($post->ID != $thispost)): ?>
+		<h2 class="postTitle"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+	<?php endif; ?>
+	<?php endwhile; ?>
+	<?php wp_reset_query(); ?>
+<?php endforeach; ?>
 
 </div>
 
